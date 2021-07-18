5fffea785309185bc2a5b58bc39d5d19606b3753
重宗大貴 <2115118a@gmail.com>
2021-07-18 18:45:43 +0900

    linux

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: hshigemu <hshigemu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 07:03:22 by hshigemu          #+#    #+#             */
-/*   Updated: 2021/07/18 18:38:59 by hshigemu         ###   ########.fr       */
+/*   Updated: 2021/07/18 18:45:33 by hshigemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,16 +60,16 @@
 # define K_ESC 65307
 # define SCROLL_UP 4
 # define SCROLL_DOWN 5
-# define KEY_1 18
-# define KEY_2 19
-# define KEY_3 20
-# define KEY_4 21
-# define KEY_5 23
-# define KEY_6 22
-# define KEY_7 26
-# define KEY_8 28
-# define KEY_9 25
-# define KEY_0 29
+# define KEY_0 48
+# define KEY_1 49
+# define KEY_2 50
+# define KEY_3 51
+# define KEY_4 52
+# define KEY_5 53
+# define KEY_6 54
+# define KEY_7 55
+# define KEY_8 56
+# define KEY_9 57
 
 #endif
 

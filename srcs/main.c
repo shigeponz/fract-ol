df8cc167f6e500e10a3749e43b302c90eab4914a
重宗大貴 <2115118a@gmail.com>
2021-08-14 09:36:06 +0900

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hshigemu <hshigemu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 07:05:26 by hshigemu          #+#    #+#             */
-/*   Updated: 2021/07/18 18:36:55 by hshigemu         ###   ########.fr       */
+/*   Updated: 2021/08/14 09:35:49 by hshigemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ int	main(int argc, char *argv[])
 	mlx_hook(env.var.win, X_EVENT_KEY_PRESS, 1L << 0, ft_keypress, &env);
 	mlx_hook(env.var.win, X_EVENT_KEY_EXIT, 0, ft_close, &env);
 	mlx_loop(env.var.mlx);
+	exit(0);
 }
